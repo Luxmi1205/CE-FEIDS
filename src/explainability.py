b821f83d874f21a0b50ca9fb5f8ebafb5998073d
Luxmi1205 <luxmixb22@gmail.com>
2026-07-01 20:52:15 +0530

    Completed SHAP visualization suite

--- src/explainability.py
+++ src/explainability.py
@@ -46,6 +46,93 @@ background = X_train_benign[:100]
 print("\nBackground dataset created successfully!")
 print(f"Background Shape: {background.shape}")
 
+# First selecting one validation sample
+sample = X_validation[:1]
+
+print("\nSample selected successfully!")
+print(f"Sample Shape: {sample.shape}")
+
+# This returns the reconstruction error for each sample.
+def anomaly_score(data):
+
+    reconstructed = model.predict(
+        data,
+        verbose=0)
+
+    errors = np.mean(
+        np.square(data - reconstructed),
+        axis=1)
+
+    return errors
+
+# Testing the function
+sample_score = anomaly_score(
+    X_validation[:5]
+)
+print("\nAnomaly scores generated successfully!")
+print(sample_score)
+
+# SHAP explainer for anomaly scores
+anomaly_explainer = shap.Explainer(
+    anomaly_score,
+    background)
+print("\nAnomaly SHAP Explainer created successfully!")
+
+anomaly_shap_values = anomaly_explainer(
+    sample
+)
+
+print("\nAnomaly SHAP values generated successfully!")
+print(anomaly_shap_values.values.shape)
+
+# Creating the waterfall plot
+plt.figure(figsize=(10, 6))
+
+shap.plots.waterfall(
+    anomaly_shap_values[0],
+    max_display=10,
+    show=False)
+
+plt.savefig(
+    "results/explainability/anomaly_score_waterfall.png",
+    dpi=300,
+    bbox_inches="tight")
+
+plt.close()
+
+print("\nAnomaly Score Waterfall plot saved successfully!")
+
+# absolute SHAP importance
+feature_importance = np.abs(
+    anomaly_shap_values.values[0])
+
+sorted_indices = np.argsort(
+    feature_importance
+)[::-1]
+
+# Creating the dataframe
+importance_df = pd.DataFrame({
+    "Feature": feature_names,
+    "SHAP Importance": feature_importance
+})
+
+# Sorting the dataframe
+importance_df = importance_df.sort_values(
+    by="SHAP Importance",
+    ascending=False
+)
+
+# displaying the top 10 features
+print("\n--------- Top 10 Important Features ----------")
+print(importance_df.head(10))
+
+# Saving the table 
+importance_df.to_csv(
+    "results/explainability/top10_shap_features.csv",
+    index=False
+)
+print("\nTop SHAP feature table saved successfully!")
+
 # Creating the SHAP explainer
 explainer = shap.Explainer(
     model,
@@ -58,12 +145,6 @@ os.makedirs( # to save results
     "results/explainability",
     exist_ok=True
 )
-
-# First selecting one validation sample
-sample = X_validation[:1]
-
-print("\nSample selected successfully!")
-print(f"Sample Shape: {sample.shape}")
 
 # Generating SHap values
 shap_values = explainer(sample)
@@ -89,3 +170,52 @@ plt.savefig(
 plt.close()
 
 print("\nSHAP Waterfall plot saved successfully!")
+
+# Now selecting multiple samples
+explanation_samples = X_validation[:100]
+
+print("\nGlobal explanation dataset created successfully!")
+print(f"Shape: {explanation_samples.shape}")
+
+# This generate Shap values
+global_shap_values = anomaly_explainer(
+    explanation_samples
+)
+
+print("\nGlobal SHAP values generated successfully!")
+print(global_shap_values.values.shape)
+
+
+# Create SHAP Summary Plot
+plt.figure(figsize=(10, 8))
+
+shap.summary_plot(
+    global_shap_values.values,
+    explanation_samples,
+    feature_names=feature_names,
+    show=False)
+
+plt.savefig(
+    "results/explainability/shap_summary_plot.png",
+    dpi=300,
+    bbox_inches="tight"
+)
+plt.close()
+
+print("\nSHAP Summary Plot saved successfully!")
+
+# Creating SHAP Global Bar Plot
+plt.figure(figsize=(10, 8))
+shap.plots.bar(
+    global_shap_values,
+    max_display=10,
+    show=False
+)
+plt.savefig(
+    "results/explainability/shap_bar_plot.png",
+    dpi=300,
+    bbox_inches="tight"
+)
+plt.close()
+
+print("\nSHAP Bar Plot saved successfully!")
